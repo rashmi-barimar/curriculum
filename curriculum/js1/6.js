80e6406470cfdaf5dfc078bb4a2203319be06a5e
rashmi barimar <rashmi34@garagescript.org>
2018-06-09 17:17:10 -0700

    solution to js1 problem 6

--- curriculum/js1/6.js
+++ curriculum/js1/6.js
@@ -5,8 +5,9 @@
  * @returns {number}
  */
 
-const solution = (a, sum=0, counter=2 ) => {
+const solution = (a) => {
   let i;
+  let sum=0;
   for (i=2; i<=a/2; i++) {
        if ( a % i === 0 ) {
            sum = sum + i;
