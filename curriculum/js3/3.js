6c422e2adb666358f66f1b3be7e615f3c7f49a5f
rashmi barimar <rashmi34@garagescript.org>
2018-06-14 13:53:05 -0700

    solution to js3 problem 3

--- curriculum/js3/3.js
+++ curriculum/js3/3.js
@@ -5,8 +5,27 @@
  */
 
 const solution = (a)=>{
-  return b;
-}
+  let dupArr = [];
+  let x;
+  let object = arrayToObject(a);
+  for ( x in object ) {
+    if ( object[x] > 1 ) {
+      dupArr.push(parseInt(x));
+    }
+  }
+  return dupArr;
+};
+const arrayToObject = (a, obj = { }, i = 0) => {
+  if ( i === a.length ) {
+    return obj;
+  }
+  if ( obj[a[i]] ) {
+    obj[a[i]]++;
+  } else {
+    obj[a[i]] = 1;
+  }
+  return arrayToObject(a, obj, i + 1);
+};
 
 module.exports = {
   solution
