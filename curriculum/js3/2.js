b41671bb9b378345c20d8de12801c9e01bcbd6a3
rashmi barimar <rashmi34@garagescript.org>
2018-06-14 12:58:38 -0700

    solution to js3 problem 2

--- curriculum/js3/2.js
+++ curriculum/js3/2.js
@@ -5,7 +5,21 @@
  */
 
 const solution = (arr)=>{
-  return 0;
+  obj = {};
+  for ( let i = 0; i < arr.length; i ++ ) {
+    if ( obj[arr[i]] ) {
+      obj[arr[i]]++;
+    } else {
+      obj[arr[i]] = 1;
+    }
+  }
+  let largest = Math.max(...(Object.values(obj)));
+  let commonNum = (Object.keys(obj)).find(function (key) {
+    if ( obj[key] === largest ) {
+      return key;
+    }
+  });
+  return parseInt(commonNum);
 };
 
 module.exports = {
