01420e852310b29ddaf8125a775a628bc3a09b00
rashmi barimar <rashmi34@garagescript.org>
2018-06-18 14:46:13 -0700

    solution to js2 problem 8

--- curriculum/js2/8.js
+++ curriculum/js2/8.js
@@ -4,16 +4,15 @@
  */
 
 const solution = () => {
-  Array.prototype.gsMap = function (a) {
-    let obj = this;
-    let newObj = [];
-    if ( obj.length === 0 ) {
-      return newObj;
-    }
-    for ( let i = 0; i < obj.length; i ++ ) {
-      newObj[i] = a(obj[i]);
-    }
+  Array.prototype.gsMap = function (callback, i = 0, newObj = []) {
+  if ( this.length === 0 ) {
     return newObj;
+  }
+  if ( i === this.length ) {
+    return newObj;
+  }
+  newObj[i] = callback(this[i]);
+  return this.gsMap(callback, i + 1, newObj);
   };
 };
 
